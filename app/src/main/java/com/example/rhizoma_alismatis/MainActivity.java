6e9be3bb5d7e8cb69ec1b06bdeed445e8a01fc86
cvrain <cvraindays@outlook.com>
2024-05-05 21:38:01 +0800

    add a navigation menu bar

--- app/src/main/java/com/example/rhizoma_alismatis/MainActivity.java
+++ app/src/main/java/com/example/rhizoma_alismatis/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.rhizoma_alismatis;
 
+import android.view.MenuItem;
+import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import com.example.rhizoma_alismatis.fragments.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    static class MyPageAdapter extends FragmentPagerAdapter {
+        List<Fragment> fragments = new ArrayList<>();
+
+        public MyPageAdapter(@NonNull @NotNull FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+            fragments.add(new PageHomeFragment());
+            fragments.add(new PageMusicListFragment());
+            fragments.add(new PageRandomMusicFragment());
+            fragments.add(new PageTunnelFragment());
+            fragments.add(new PageUserFragment());
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull @NotNull View view, @NonNull @NotNull Object object) {
+            return view == object;
+        }
+    }
+
+    MyPageAdapter fragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.container_main);
+        fragmentAdapter = new MyPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(fragmentAdapter);
+        viewPager.setCurrentItem(0);
+    }
+
+    public void gotoHome(MenuItem item) {
+        viewPager.setCurrentItem(0);
+    }
+
+    public void gotoMusicList(MenuItem item) {
+        viewPager.setCurrentItem(1);
+    }
+
+    public void gotoPlay(MenuItem item) {
+        viewPager.setCurrentItem(2);
+    }
+
+    public void gotoTunnel(MenuItem item) {
+        viewPager.setCurrentItem(3);
+    }
+
+    public void gotoUser(MenuItem item) {
+        viewPager.setCurrentItem(4);
     }
 }
---
+++ app/src/main/java/com/example/rhizoma_alismatis/fragments/PageRandomMusicFragment.java
@@ -0,0 +1,64 @@
+package com.example.rhizoma_alismatis.fragments;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.rhizoma_alismatis.R;
+import org.jetbrains.annotations.NotNull;
+
+public class PageRandomMusicFragment extends Fragment {
+    private static final String TAG = "page_random_play";
+    View myView;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @SuppressLint("InflateParams")
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        //防止在滑动组件之后销毁滑动前的组件,并且可以保证保留之前的组件状态
+        if (myView == null) {
+            myView = inflater.inflate(R.layout.layout_random_music, null);
+        }
+        clearParent(myView);
+        Log.d(TAG, "onCreateView: ");
+        return myView;
+    }
+
+    private void clearParent(View v) {
+        ViewGroup pa = (ViewGroup) v.getParent();
+        if (pa != null) {
+            pa.removeView(v);
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull @NotNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
